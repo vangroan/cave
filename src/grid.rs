9f882bf689cb8f237b5bc9ec176ef04d29f3f4a9
Willem Victor <wimpievictor@gmail.com>
2019-03-10 19:42:24 +0200

    Attempt at iterating neighbours

--- src/grid.rs
+++ src/grid.rs
@@ -1,6 +1,18 @@
 
 use na::{Vector3};
 
+// TODO: Make 3D
+const OFFSETS : [Vector3<i32>; 8] = [
+    Vector3::new(-1, -1, 0),
+    Vector3::new(0, -1, 0),
+    Vector3::new(1, -1, 0),
+    Vector3::new(-1, 0, 0),
+    Vector3::new(1, 0, 0),
+    Vector3::new(-1, 1, 0),
+    Vector3::new(0, 1, 0),
+    Vector3::new(1, 1, 0),
+];
+
 /// 3-Dimensional Grid
 pub struct Grid {
     size: Vector3<u32>,
@@ -12,13 +24,77 @@ impl Grid {
             size: Vector3::new(x, y, z),
         }
     }
+
+    pub fn neighbours(&self, pos: &GridPosition) -> Neighbours {
+        let mut n = Neighbours([None, None, None, None, None, None, None, None]);
+
+        for i in 0..8 {
+            let v = GridPosition(pos.0 + OFFSETS[i]);
+            n.0[i] = if self.in_bounds(&v) {
+                Some(v)
+            } else {
+                None
+            }
+        }
+
+        n
+    }
+
+    fn in_bounds(&self, pos: &GridPosition) -> bool {
+        pos.x() >= 0 && pos.x() < self.size.x as i32 && pos.y() >= 0 && pos.y() < self.size.y as i32
+    }
 }
 
-#[derive(Clone, Debug)]
+pub struct Neighbours([Option<GridPosition>; 8]);
+
+impl Neighbours {
+    pub fn into_iter<'a>(self) -> Iter<'a>{
+        Iter::new(self.0.into_iter().filter_map(|maybe_pos| *maybe_pos))
+    }
+}
+
+pub struct Iter<'i> {
+    // TODO: Remove heap alloc
+    inner: &'i mut Iterator<Item = GridPosition>,
+}
+
+impl<'i> Iter<'i> {
+    fn new<I>(inner: I) -> Self
+    where
+        I: Iterator<Item = GridPosition> + 'i
+    {
+        Iter {
+            inner: &mut inner
+        }
+    }
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = GridPosition;
+
+    fn next(&mut self) -> Option<GridPosition> {
+        self.inner.next()
+    }
+}
+
+
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
 pub struct GridPosition(Vector3<i32>);
 
 impl GridPosition {
     pub fn new(x: i32, y: i32, z: i32) -> GridPosition {
         GridPosition(Vector3::new(x, y, z))
+    }
+
+    pub fn x(&self) -> i32 {
+        self.0.x
+    }
+
+    pub fn y(&self) -> i32 {
+        self.0.y
+    }
+
+    pub fn z(&self) -> i32 {
+        self.0.z
     }
 }
--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -3,7 +3,12 @@ use specs::{System, Write, Read};
 
 use super::grid::{Grid, GridPosition};
 
-use std::collections::VecDeque;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashSet, VecDeque};
+
+fn manhatten(a: &GridPosition, b: &GridPosition) -> u32 {
+    ((a.x() - b.x()).abs() + (a.y() - b.y()).abs() + (a.z() - b.z()).abs()) as u32
+}
 
 pub struct Pathfinder;
 
@@ -13,7 +18,28 @@ impl Pathfinder {
     }
 
     pub fn find_path(&self, grid: &Grid, start: &GridPosition, end: &GridPosition) {
+        // Note the BinaryHeap is a max-heap
+        let mut open : BinaryHeap<PathNode> = BinaryHeap::new();
+        let mut close : HashSet<GridPosition> = HashSet::new();
 
+        // Seed lists with initial position
+        let start_h = manhatten(start, end);
+        open.push(PathNode {
+            pos :start.clone(),
+            parent: None,
+            g: 0,
+            h: start_h,
+            cost: start_h,
+        });
+        close.insert(start.clone());
+
+
+        while let Some(node) = open.pop() {
+
+            for neigh in grid.neighbours(&node.pos).into_iter() {
+
+            }
+        }
     }
 }
 
@@ -25,15 +51,29 @@ impl Default for Pathfinder {
 
 pub struct Path(Vec<PathNode>);
 
+#[derive(Eq, PartialEq)]
 pub struct PathNode {
     pub pos: GridPosition,
-    pub parent: GridPosition,
+    pub parent: Option<GridPosition>,
     /// distance from start
     pub g: u32,
     /// heuristic
     pub h: u32,
     /// total cost
     pub cost: u32,
+}
+
+impl Ord for PathNode {
+    fn cmp(&self, other: &PathNode) -> Ordering {
+        // Note that this is backwards to allow for a min-heap
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for PathNode {
+    fn partial_cmp(&self, other: &PathNode) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[derive(Default)]
@@ -70,7 +110,5 @@ impl<'a> System<'a> for PathfindingSystem {
         use rayon::prelude::*;
         use specs::Join;
         use specs::ParJoin;
-
-
     }
 }

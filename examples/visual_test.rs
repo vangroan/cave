3b076b6e3da59e7e0ff133b2d447de5820fd6914
Willem Victor <wimpievictor@gmail.com>
2019-04-06 22:17:54 +0200

    Set resolution

--- examples/visual_test.rs
+++ examples/visual_test.rs
@@ -10,7 +10,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let settings = Settings::default()
         .title("Visual Test")
-        // .window_size(800, 600)
+        .window_size(800, 600)
         .fps(60)
         .ups(60);
 
